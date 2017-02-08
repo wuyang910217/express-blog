009bbe02cc84135a4d105354339e6503e03e13fa
wuyang <wuyang910217@gmail.com>
2017-02-08 15:18:43 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
 
+app.set('views', path.join(__dirname,  'views'));
+app.set('view engine', 'ejs');
+
 app.use('/', indexRouter);
 app.use('/users', userRouter);
+
+app.use(function(req, res, next) {
+  res.status(404).render('404');
+});
 
 app.listen(3000, function() {
   console.log('app runs at localhost:3000');
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/:name', function(req, res) {
-  res.send('hello' + req.params.name);
+  res.render('users', { name: req.params.name });
 });
 
 module.exports = router;
